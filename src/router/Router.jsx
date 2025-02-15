46abb811a54911f0e34d60a32a7b3d9ae87e8f7e
abdulmoiz180 <moiza8684@gmail.com>
2025-02-15 18:42:28 +0500

    fixed navbar

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Home from '../pages/Home/index.jsx';
 import Footer from '../layout/Footer/index.jsx';
+import Navbar from '../layout/NavBar/index.jsx';
 const Router = () => {
   return (
     <BrowserRouter>
